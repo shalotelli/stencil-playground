4c3f23ce7a225eb9cd8fd82136ff0b210badfb18
Sha Alibhai <shalotelli@gmail.com>
2019-11-06 08:30:39 -0500

    load content from jsonplaceholder api

--- src/components/random-post/random-post.tsx
+++ src/components/random-post/random-post.tsx
@@ -1,29 +1,52 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Element } from '@stencil/core';
 
 @Component({
   tag: 'random-post',
-  styleUrl: 'random-post.css',
+  styleUrl: 'random-post.scss',
   shadow: true
 })
 export class RandomPost {
+  @Element() el: HTMLElement;
   content: any;
 
   componentWillLoad() {
-    console.log('testing')
-
-    // https://jsonplaceholder.typicode.com/
-    return fetch('https://jsonplaceholder.typicode.com/posts/1')
+    return fetch('https://jsonplaceholder.typicode.com/comments/1')
       .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        this.content = data;
-      })
+      .then(data => this.content = data);
+  }
+
+  componentDidLoad() {
+    const timeSince = this.el.shadowRoot.querySelector('time-since');
+    const secondsToCount = 10;
+    let i = 0;
+
+    const countInterval = setInterval(() => {
+      const timestamp = new Date;
+
+      if (i < secondsToCount) {
+        i += 1;
+
+        timestamp.setSeconds(timestamp.getSeconds() - i);
+        timeSince.timestamp = timestamp;
+      } else {
+        clearInterval(countInterval);
+      }
+    }, 1000);
   }
 
   render() {
     return (
       <Host>
-        Random Post
+        <div class="header">
+          <img src="https://placeimg.com/300/300/animals" />
+          <div class="username">{this.content.name}</div>
+        </div>
+
+        <div class="content">{this.content.body}</div>
+
+        <div class="footer">
+          <span>Posted</span> <time-since unit="second"></time-since>
+        </div>
       </Host>
     );
   }
